b6087febf65f712d7b1797b966ac8129e6647e4d
giedockx <dockx.gie@gmail.com>
2023-02-19 15:23:50 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,19 +28,25 @@ export default defineConfig({
             create: true,
             delete: true,
             fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'title', widget: 'string', label: 'Post Title' },              
+              {
+                name: 'publishDate',
+                widget: 'datetime',
+                format: 'DD MMM YYYY',
+                date_format: 'DD MMM YYYY',
+                time_format: false,
+                label: 'Publish Date',
+              },
+              { name: 'author', widget: 'string', label: 'Author Name', required: false },
+              { name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
+              { name: 'description', widget: 'string', label: 'Description', required: false },
               { name: 'body', widget: 'markdown', label: 'Post Body' },
-            ]
-          },
-          {
-            name: 'webPages',
-            label: "Web Pagina's",
-            folder: 'src/pages',
-            create: true,
-            delete: true,
-            fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
+              {
+                name: 'layout',
+                widget: 'select',
+                default: 'layouts/BaseLayout.astro',
+                options: [
+                  { label: 'Blog Post', value: 'layouts/BaseLayout.astro' },
             ]
           }
         ],
